baf974ef77f107a7138e41faaca804b5b71721dc
Kelsey Dickson <kelseydickson@Kelseys-MBP.fios-router.home>
2022-12-15 18:54:37 -0500

    added react project

---
+++ morning/src/components/Home.js
@@ -0,0 +1,15 @@
+import Nav from "./Nav/Nav";
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
+const Home = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
